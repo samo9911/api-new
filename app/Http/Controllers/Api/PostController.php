46cb7e6ee367591fd20e56650c131af08434ced3
Samuel <jos90500@gmail.com>
2021-02-26 01:04:59 -0600

    Union con post
    validamos el controlador con el postrequests

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\Post as PostRequests;
 
 class PostController extends Controller
 {
@@ -29,7 +29,7 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostRequests $request)
     {
         $post = $this->post->create($request->all());
 
