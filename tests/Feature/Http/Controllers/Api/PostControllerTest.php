58279169dbc05c2af740212f4df826992832cec4
Samuel <jos90500@gmail.com>
2021-02-26 00:47:05 -0600

    video 1-4
    Por ciertos errores tengo que subir del video1 al 4

---
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_store()
+    {
+       // $this->withoutExceptionHandLing();
+        $response = $this->json('POST', '/api/posts',[
+            'title' => 'El post de prueba'
+        ]);
+        
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+        ->assertJson(['title' => 'El post de prueba'])
+        ->assertStatus(201);
+
+        $this->assertDatabaseHas('posts', ['title' => 'El post de prueba']);
+    }
+}
